cacad1f17df9c1f14e9e2dd5901fb5c79a88fb74
Justin <justinereifanwijaya@gmail.com>
2026-06-12 19:31:26 +0700

    modif: title change

--- resources/js/pages/cycle2/agitation-test-2.tsx
+++ resources/js/pages/cycle2/agitation-test-2.tsx
@@ -52,9 +52,7 @@ export default function AgitationTest2() {
     return (
         <>
             <Head>
-                <title>
-                    Cycle 2 - AgitationSection 2 | Full Bright Indonesia
-                </title>
+                <title>Kelas TOEFL Skor 500+ Untuk LPDP dan Kerja</title>
                 <meta
                     name="description"
                     content="Testing AgitationSection 2 variant"
